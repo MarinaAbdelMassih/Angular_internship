c5c42d97ac4fe809889942aeed36b12ba83d1fca
MarinaAbdelMassih <mabdelmassih@panarab-media.com>
2020-10-21 00:39:54 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemComponent } from './content/item/item.component';
+import { ListComponent } from './content/list/list.component';
+import { MainComponent } from './content/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ListComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
